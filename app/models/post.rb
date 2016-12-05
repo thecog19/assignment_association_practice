033ac8427d94a96c2cffa6f2690f937b62f289fe
Felipe Suero <suerofrancisco@yahoo.com>
2016-12-04 21:20:27 -0500

    some practice

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :users,  :through => :userpost
   has_many :comments, :dependent => :destroy 
   has_many :posttaggings, :through => :posttags, :source => :tag
-  has_many :whothefuck, :through => :posttags, :source => :tag
+  has_many :whothedang, :through => :posttags, :source => :tag
   belongs_to :category
   belongs_to :fireplace, :class_name => "Category", :foreign_key => :category_id
   has_many :posttags
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :userposts
-  has_many :ugly_motherfuckers, :through => :userposts, :source => :post
+  has_many :ugly_guys, :through => :userposts, :source => :post
   has_many :comments, :foreign_key => :user_id, :dependent => :destroy 
 end
